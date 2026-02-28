b2cbc7dc5266ba79edd6052628f8f94ec244fb23
savaniravi <savaniravik007@gmail.com>
2026-02-28 17:33:42 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/portfolio/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
